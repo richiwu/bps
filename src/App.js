c6e7f0d1698d24b46b6ad52b8801820f7412c5fc
richiwu <richard_wu94@hotmail.com>
2018-10-04 22:24:04 -0400

    lawl

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+      super(props);
+      this.state = {
+        percentage: '',
+        bps: ''
+      }
+      this.handleChangePercent = this.handleChangePercent.bind(this);
+      this.handleChangeBps = this.handleChangeBps.bind(this);
+  }
+  
+
+  handleChangePercent (event) {
+    this.setState({
+        percentage: event.target.value
+    })
+  }
+
+  handleChangeBps (event) {
+    this.setState({
+        bps: event.target.value
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="dank">
+          <h1> BPS to Percentage </h1>
+          <p>Enter BPS Value </p>
+          <input type="text" name="percent" value={this.state.percentage} onChange = {this.handleChangePercent} />
+          <h3>{this.state.percentage/100} % </h3>
+
+          <h1> Percentage to BPS </h1>
+          <p> Enter Percentage Value </p>
+          <input type="text" name="bps" value={this.state.bps} onChange = {this.handleChangeBps} />  
+          <h3>{this.state.bps*100} bps </h3>
+        </div> 
       </div>
     );
   }
